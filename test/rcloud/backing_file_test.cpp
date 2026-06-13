c631f24a664d7adddc9fd221b4af2344d061d9df
Patman1O1 <patrickthomas101x@gmail.com>
2026-06-13 16:57:52 -0500

    Implement backing_file_is_reg.is_directory

--- test/rcloud/backing_file_test.cpp
+++ test/rcloud/backing_file_test.cpp
@@ -141,6 +141,17 @@ namespace backing_file_testing {
         std::filesystem::remove_all(TEST_FILE_PATH);
     }
 
+    TEST(backing_file_is_reg, is_symlink) {
+        // Ensure the symlink doesn't already exist
+        std::filesystem::remove(TEST_FILE_PATH);
+
+        EXPECT_NE(-1, ::symlink(std::getenv("HOME"), TEST_FILE_PATH));
+        EXPECT_FALSE(::backing_file_is_reg(TEST_FILE_PATH));
+
+        // Remove the symlink
+        std::filesystem::remove(TEST_FILE_PATH);
+    }
+
     // ── Function Tests (backing_file_create) ─────────────────────────────────────────────────────────────────────────
     TEST(backing_file_create, backing_file_nullptr) {
         EXPECT_EQ(-1, ::backing_file_create(nullptr, "no/such/path", 0));
