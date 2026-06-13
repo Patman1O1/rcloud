f455bdb76b4f8988860e19c2722a2a034f11af5c
Patman1O1 <patrickthomas101x@gmail.com>
2026-06-13 14:49:01 -0500

    Add loop.c

---
+++ src/rcloud/loop.c
@@ -0,0 +1,14 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif // #ifndef _GNU_SOURCE
+
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+
