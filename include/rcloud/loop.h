9c5eabd876089158cc8c23f9ff9052db5bfc4306
Patman1O1 <patrickthomas101x@gmail.com>
2026-06-13 14:48:38 -0500

    Add loop.h

---
+++ include/rcloud/loop.h
@@ -0,0 +1,15 @@
+#ifndef LOOP_H
+#define LOOP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+
+#endif // #ifndef LOOP_H
+
