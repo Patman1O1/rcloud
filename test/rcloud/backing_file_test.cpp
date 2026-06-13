a2771e614a131c7e4ba68917fb2b5f190e2c1083
Patman1O1 <patrickthomas101x@gmail.com>
2026-06-12 19:41:35 -0500

    Edit backing_file_test.cpp

--- test/rcloud/backing_file_test.cpp
+++ test/rcloud/backing_file_test.cpp
@@ -1,3 +1,47 @@
-//
-// Created by patri on 6/11/2026.
-//
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif // #ifndef _GNU_SOURCE
+
+// ISO C++ Includes
+#include <filesystem>
+
+// ISO C Includes
+
+// POSIX Includes
+
+// GNU Includes
+
+// Third Party Includes
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Local Includes
+#include <rcloud/backing_file.h>
+
+namespace backing_file_testing {
+    TEST(backing_file_create, backing_file_nullptr) {
+        EXPECT_EQ(-1, ::backing_file_create(nullptr, "no/such/path", 0));
+        EXPECT_EQ(EFAULT, errno);
+    }
+
+    TEST(backing_file_create, path_nullptr) {
+        struct ::backing_file file;
+        EXPECT_EQ(-1, ::backing_file_create(&file, nullptr, 0));
+        EXPECT_EQ(EFAULT, errno);
+    }
+
+    TEST(backing_file_create, non_positive_size) {
+        struct ::backing_file file;
+        EXPECT_EQ(-1, ::backing_file_create(&file, "no/such/path", -1));
+        EXPECT_EQ(EINVAL, errno);
+
+        // Reset errno
+        errno = 0;
+
+        EXPECT_EQ(-1, ::backing_file_create(&file, "no/such/path", 0));
+        EXPECT_EQ(EINVAL, errno);
+    }
+
+    
+} // namespace backing_file_testing
+
