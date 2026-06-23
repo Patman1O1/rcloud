fd7b20c05eb5567f7c155f74c17b9da7bedabce6
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-23 13:30:26 -0500

    Include headers

--- src/rcloud/drive.c
+++ src/rcloud/drive.c
@@ -4,12 +4,20 @@
 
 // ISO Includes
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 // POSIX Includes
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mount.h>
+#include <sys/ioctl.h>
 
 // GNU Includes
+#include <linux/loop.h>
 
 // Local Includes
 #include "rcloud/drive.h"
