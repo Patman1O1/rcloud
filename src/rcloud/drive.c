faafd512553ee73e41f0e9aa2abc8d42439f7c1f
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-23 13:16:43 -0500

    Add drive.c

---
+++ src/rcloud/drive.c
@@ -0,0 +1,16 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif // #ifndef _GNU_SOURCE
+
+// ISO Includes
+#include <stdlib.h>
+
+
+// POSIX Includes
+
+
+// GNU Includes
+
+// Local Includes
+#include "rcloud/drive.h"
+
