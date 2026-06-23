ba4148b3c9f73de3aa7edc761c365d78ef9825f9
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-23 13:59:16 -0500

    Implement gethome()

--- src/rcloud/drive.c
+++ src/rcloud/drive.c
@@ -11,6 +11,7 @@
 // POSIX Includes
 #include <unistd.h>
 #include <fcntl.h>
+#include <pwd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
@@ -22,3 +23,31 @@
 // Local Includes
 #include "rcloud/drive.h"
 
+static const char* gethome(char* result_buf, const size_t result_size) {
+    if (result_buf == nullptr) {
+        errno = EFAULT;
+        return nullptr;
+    }
+
+    struct passwd pw;
+    struct passwd* result;
+
+    // Allocate temporary space for the database lookup
+    char buffer[1024];
+
+    if (getpwuid_r(getuid(), &pw, buffer, sizeof(buffer), &result) != 0 || result == nullptr) {
+        return nullptr;
+    }
+
+    // Ensure the result buffer is large enough to hold the user's home directory path
+    const size_t pw_dir_len = strlen(pw.pw_dir);
+    if (pw_dir_len + 1 > result_size) {
+        errno = ENAMETOOLONG;
+        return nullptr;
+    }
+
+    // Copy the home directory into the result buffer
+    memcpy(result_buf, pw.pw_dir, pw_dir_len);
+    result_buf[pw_dir_len] = '\0';
+    return result_buf;
+}
