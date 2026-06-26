e753cb326cf24dc388d75983fece721e87a262e0
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-26 16:38:18 -0500

    Implement drive.h

--- include/rcloud/drive.h
+++ include/rcloud/drive.h
@@ -5,9 +5,13 @@
 extern "C" {
 #endif // #ifdef __cplusplus
 
-extern int create_drive(void);
+extern char* rcloud_drive_img_path(void);
 
-extern int destroy_drive(void);
+extern char* rcloud_drive_mnt_path(void);
+
+extern int rcloud_drive_create(const char* img_path, const char* mnt_path);
+
+extern int rcloud_drive_destroy(const char* img_path, const char* mnt_path);
 
 #ifdef __cplusplus
 }
